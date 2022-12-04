6bdf6c4f6e45498a54b8ba26533ede31853b00b8
Heuler Manfredi <felipemanfredigalaxy@gmail.com>
2022-12-04 18:04:45 -0300

    first commit

---
+++ src/main/kotlin/com/stack/kotlinaws/KotlinAwsApplication.kt
@@ -0,0 +1,11 @@
+package com.stack.kotlinaws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAwsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAwsApplication>(*args)
+}
---
+++ src/test/kotlin/com/stack/kotlinaws/KotlinAwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stack.kotlinaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAwsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
